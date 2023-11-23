6750741e6d9ae79a08496223493d90201a290011
Noan Cloarec <noan.cloarec@zenika.com>
2023-11-23 16:57:09 +0100

    Draft : working resize with vue leaflet but anchor does not work

--- src/components/ParisMap.vue
+++ src/components/ParisMap.vue
@@ -6,6 +6,8 @@ import Chart from 'chart.js/auto';
 import "leaflet/dist/leaflet.css";
 
 import { onMounted, reactive, ref, watch } from "vue";
+const circleSize = ref(200)
+const map = ref(null)
 onMounted(() => {
 
     // const chartSize = ref(200)
@@ -31,6 +33,7 @@ onMounted(() => {
         addPie()
     }, 1000)
 
+      console.log(map.value)
     // map.on('zoomend', function() {
     //     const zoom = map.getZoom();
     //     console.log(zoom)
@@ -43,6 +46,7 @@ onMounted(() => {
 
 })
 const pie = ref(null)
+
 
 const addPie = () => {
 
@@ -71,49 +75,21 @@ const addPie = () => {
     })
 }
 const zoom = ref(12)
-
-// watch([pie, zoom], ([a, b], [oldA, olB]) => {
-//     console.log({a, b, oldA, olB})
-//     console.log(a.isSameNode(oldA))
-//     console.log(a?.toDataURL())
-// })
-let canvases = []
-setInterval(() => {
-  const newCanvases = Array.from(document.querySelectorAll("canvas"))
-  for(let i = 0; i < canvases.length; i++) {
-    if(!canvases[i].isSameNode(newCanvases[i])) {
-        console.log(newCanvases[i].toDataURL())
-        console.log(canvases[i].toDataURL())
-    }
-  }
-  canvases = newCanvases
-
-}, 1000)
-
-const makeHtml = () => {
-    return `<div class="chart-container"><canvas id="pie"></canvas></div>`
+const zoomUpdated = (newZoom) => {
+  circleSize.value = 13*newZoom;
+  console.log(circleSize.value)
+  document.getElementsByClassName("chart-container")[0].style.width = `${circleSize.value}px`
+  document.getElementsByClassName("chart-container")[0].style.height = `${circleSize.value}px`
 }
-
-const staticAnchor = reactive([100, 100])
-const customText = ref("Hello World")
-const data = reactive({
-            labels: ["At least 1 male and 1 female", "At least 1 female", "No domestic - Boss or employee", "No domestic - Worker"],
-            datasets: [{
-                label: 'Passy',
-                data: [10, 20, 30, 40],
-                backgroundColor: ['#cc766e', '#d9ae3c', '#7b9687','#72635d'],
-                borderColor: '#000',
-                borderWidth: .5
-            }],
-        })
+      
 
 
 </script>
 
 <template>
     <!-- <img src="src/assets/paris_bien_etre.jpg" /> -->
-    <div style="height:600px; width:800px">
-    <l-map ref="map" v-model:zoom="zoom" :center="[48.913749, 2.225493]">
+    <div id="map-container">
+    <l-map ref="map" :zoom="zoom" :center="[48.913749, 2.225493]" @update:zoom="zoomUpdated">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
@@ -121,8 +97,7 @@ const data = reactive({
       ></l-tile-layer>
       <l-marker :lat-lng="[48.879411, 2.290932]">
         <l-icon
-          :icon-anchor="[100, 100]"
-          :icon-size="[200, 200]"
+          :icon-anchor="[200/2, 200/2]"
           class-name="someExtraClass"
         >
           <div class="chart-container">
@@ -146,5 +121,6 @@ div {
 .chart-container{
   border: 2px solid red;
   height: 200px;
+  transition: width 0.5s, height 0.5s;
 }
 </style>
