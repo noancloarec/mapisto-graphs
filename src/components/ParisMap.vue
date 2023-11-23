1106fa958cc261cf0829d43b95d93ea4c1ae2106
Noan Cloarec <noan.cloarec@zenika.com>
2023-11-23 14:20:47 +0100

    Draft: bugged version with vue leaflet

--- src/components/ParisMap.vue
+++ src/components/ParisMap.vue
@@ -1,51 +1,53 @@
 
 <script setup>
-import L from "leaflet";
+import { LMap, LTileLayer ,LMarker, LIcon, LImageOverlay} from "@vue-leaflet/vue-leaflet";
 import Chart from 'chart.js/auto';
 import "leaflet/dist/leaflet.css";
 
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 onMounted(() => {
 
     // const chartSize = ref(200)
 
-    const divIcon = L.divIcon({
-        html: makeHtml(2),
-        className: 'divIcon',
-        iconSize: [200, 200],
-        iconAnchor: [100, 100]
-        });
+    // const divIcon = L.divIcon({
+    //     html: makeHtml(2),
+    //     className: 'divIcon',
+    //     iconSize: [200, 200],
+    //     iconAnchor: [100, 100]
+    //     });
 
-    const map = L.map('map-container').setView([48.913749, 2.225493], 12);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    const latLngBounds = L.latLngBounds([[48.913, 2.205], [48.802, 2.428]]);
-    const imageOverlay = L.imageOverlay('/src/assets/paris_bien_etre_small.jpg', latLngBounds, {opacity : 1}).addTo(map);
-    const marker = L.marker([48.879411, 2.290932], {
-        icon: divIcon,
-    }).addTo(map);
+    // const map = L.map('map-container').setView([48.913749, 2.225493], 12);
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     maxZoom: 19,
+    //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    // }).addTo(map);
+    // const latLngBounds = L.latLngBounds([[48.913, 2.205], [48.802, 2.428]]);
+    // const imageOverlay = L.imageOverlay('/src/assets/paris_bien_etre_small.jpg', latLngBounds, {opacity : 1}).addTo(map);
+    // const marker = L.marker([48.879411, 2.290932], {
+    //     icon: divIcon,
+    // }).addTo(map);
     setTimeout(() => {
         addPie()
     }, 1000)
 
-    map.on('zoomend', function() {
-        const zoom = map.getZoom();
-        console.log(zoom)
-        const size = 13*zoom
-        console.log(divIcon)
-        document.getElementsByClassName("chart-container")[0].style.width = `${size}px`
-        document.getElementsByClassName("chart-container")[0].style.height = `${size}px`
-        console.log(divIcon.options.iconSize)
-    });
+    // map.on('zoomend', function() {
+    //     const zoom = map.getZoom();
+    //     console.log(zoom)
+    //     const size = 13*zoom
+    //     console.log(divIcon)
+    //     document.getElementsByClassName("chart-container")[0].style.width = `${size}px`
+    //     document.getElementsByClassName("chart-container")[0].style.height = `${size}px`
+    //     console.log(divIcon.options.iconSize)
+    // });
 
 })
+const pie = ref(null)
 
 const addPie = () => {
-    document.getElementsByClassName("chart-container")[0].style.transition = "width 0.5s, height 0.5s";
-    document.getElementsByClassName("chart-container")[0].style.border = "1px solid red";
-    const ch = new Chart(document.getElementById("pie"), {
+
+  const pie = document.getElementById("pie")
+
+    const ch = new Chart(pie, {
         type: 'pie',
         data: {
             labels: ["At least 1 male and 1 female", "At least 1 female", "No domestic - Boss or employee", "No domestic - Worker"],
@@ -57,26 +59,75 @@ const addPie = () => {
                 borderWidth: .5
             }],
         },
+        
         options: {
-            plugins: {
-                legend: {
-                    display: false
-                }
-            }
+          plugins : {
+            legend: {
+              display: false
+            },
+          }
         }
     })
 }
+const zoom = ref(12)
+
+watch([pie, zoom], ([a, b], [oldA, olB]) => {
+    console.log({a, b, oldA, olB})
+    console.log(a.isSameNode(oldA))
+    console.log(a?.toDataURL())
+})
+let canvases = []
+setInterval(() => {
+  const newCanvases = Array.from(document.querySelectorAll("canvas"))
+  for(let i = 0; i < canvases.length; i++) {
+    console.log(canvases[i].isSameNode(newCanvases[i]))
+  }
+  canvases = newCanvases
+
+}, 1000)
+
 const makeHtml = () => {
     return `<div class="chart-container"><canvas id="pie"></canvas></div>`
 }
+
+const staticAnchor = reactive([100, 100])
+const customText = ref("Hello World")
+const data = reactive({
+            labels: ["At least 1 male and 1 female", "At least 1 female", "No domestic - Boss or employee", "No domestic - Worker"],
+            datasets: [{
+                label: 'Passy',
+                data: [10, 20, 30, 40],
+                backgroundColor: ['#cc766e', '#d9ae3c', '#7b9687','#72635d'],
+                borderColor: '#000',
+                borderWidth: .5
+            }],
+        })
 
 
 </script>
 
 <template>
     <!-- <img src="src/assets/paris_bien_etre.jpg" /> -->
-    <div id="map-container">
-    </div>
+    <div style="height:600px; width:800px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[48.913749, 2.225493]">
+      <!-- <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer> -->
+      <l-marker :lat-lng="[48.879411, 2.290932]">
+        <l-icon
+          :icon-anchor="[100, 100]"
+          :icon-size="[200, 200]"
+          class-name="someExtraClass"
+        >
+          <div class="chart-container">
+            <canvas id="pie" ref="pie"></canvas>
+          </div>
+        </l-icon>
+      </l-marker>
+    </l-map>
+  </div>
 </template>
 
 <style scoped>
@@ -89,12 +140,7 @@ div {
     width: 100vw;
 }
 .chart-container{
-    width: 200px;
-    height: 200px;
-    border: 2px solid red;
-    transition:
-        width 0.5s,
-        height 0.5s; 
-
+  border: 2px solid red;
+  height: 200px;
 }
 </style>
