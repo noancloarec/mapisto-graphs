76f7516911c6a6b0a747ca670e60f0e8d1573bdc
Noan Cloarec <noan.cloarec@zenika.com>
2023-11-23 09:57:23 +0100

    Draft: Display paris map on osm

---
+++ src/components/ParisMap.vue
@@ -0,0 +1,36 @@
+
+<script setup>
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import { onMounted } from "vue";
+onMounted(() => {
+    const map = L.map('map-container').setView([48.913749, 2.225493], 12);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    const latLngBounds = L.latLngBounds([[48.913, 2.205], [48.802, 2.428]]);
+    const imageOverlay = L.imageOverlay('/src/assets/paris_bien_etre.jpg', latLngBounds, {opacity : 1}).addTo(map);
+
+})
+
+
+</script>
+
+<template>
+    <!-- <img src="src/assets/paris_bien_etre.jpg" /> -->
+    <div id="map-container">
+    </div>
+</template>
+
+<style scoped>
+div {
+    color: red;
+}
+
+#map-container {
+    height: 80vh;
+    width: 100vw;
+}
+</style>
