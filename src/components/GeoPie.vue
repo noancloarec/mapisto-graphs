8510076c24b366c4b9593cf4d03f105949e43f73
Noan Cloarec <noan.cloarec@zenika.com>
2023-11-25 00:47:40 +0100

    Draft : many pies

---
+++ src/components/GeoPie.vue
@@ -0,0 +1,13 @@
+<template>
+                <l-marker v-for="(pie, index) in pies" :key="pie.title" :lat-lng="[pie.latitude, pie.longitude]"
+                :draggable="true" @update:lat-lng="position => circlePositionUpdated(pie, position)"
+                @click="() => selectPie(index)">
+                <l-icon :icon-size="[getCircleSizeInPixels(pie.sizeInMeters), getCircleSizeInPixels(pie.sizeInMeters)]"
+                    :icon-anchor="[getCircleSizeInPixels(pie.sizeInMeters) / 2, getCircleSizeInPixels(pie.sizeInMeters) / 2]"
+                    class-name="someExtraClass">
+                    <div class="chart-container">
+                        <canvas :id="pie.title" ref="pie"></canvas>
+                    </div>
+                </l-icon>
+            </l-marker>
+</template>
--- src/components/ParisMap.vue
+++ src/components/ParisMap.vue
@@ -1,13 +1,15 @@
 
 <script setup>
-import L from "leaflet";
-import { LMap, LTileLayer ,LMarker, LIcon, LImageOverlay} from "@vue-leaflet/vue-leaflet";
+import L, { LatLng } from "leaflet";
+import piesData from '../assets/pies.json'
+import { LMap, LTileLayer, LMarker, LIcon, LImageOverlay } from "@vue-leaflet/vue-leaflet";
 import Chart from 'chart.js/auto';
 import "leaflet/dist/leaflet.css";
 
 import { onMounted, reactive, ref, watch } from "vue";
-const circleSize = ref(200)
-const map = ref(null)
+
+const pies = reactive(piesData)
+
 onMounted(() => {
 
     // const chartSize = ref(200)
@@ -33,7 +35,7 @@ onMounted(() => {
         addPie()
     }, 1000)
 
-      console.log(map.value)
+    console.log(map.value)
     // map.on('zoomend', function() {
     //     const zoom = map.getZoom();
     //     console.log(zoom)
@@ -46,67 +48,139 @@ onMounted(() => {
 
 })
 const pie = ref(null)
+let ch = []
+const nb_1_male_1_female = ref(0)
 
 
 const addPie = () => {
-
-  const pie = document.getElementById("pie")
-
-    const ch = new Chart(pie, {
-        type: 'pie',
-        data: {
-            labels: ["At least 1 male and 1 female", "At least 1 female", "No domestic - Boss or employee", "No domestic - Worker"],
-            datasets: [{
-                label: 'Passy',
-                data: [10, 20, 30, 40],
-                backgroundColor: ['#cc766e', '#d9ae3c', '#7b9687','#72635d'],
-                borderColor: '#000',
-                borderWidth: .5
-            }],
-        },
-        
-        options: {
-          plugins : {
-            legend: {
-              display: false
+    ch.forEach((item) => item.destroy())
+    ch = []
+    pies.forEach((pie) => {
+        const canvas = document.getElementById(pie.title)
+        ch.push(new Chart(canvas, {
+            type: 'pie',
+            data: {
+                labels: pie.data.map((item) => item.label),
+                datasets: [{
+                    label: pie.title,
+                    data: pie.data.map((item) => item.value),
+                    backgroundColor: pie.data.map((item) => item.backgroundColor),
+                    borderColor: '#000',
+                    borderWidth: .5
+                }],
             },
-          }
-        }
+            options: {
+                rotation: 180,
+                layout: {
+                    padding: {
+                        right: 110,
+                        left: 110,
+                        top: 0,
+                        bottom: 0
+                    }
+                },
+                animation: {
+                    duration: 0
+                },
+
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                }
+            }
+        }))
     })
 }
 const zoom = ref(12)
+
+const map = ref(null)
+
 const zoomUpdated = (newZoom) => {
-  circleSize.value = 13*newZoom;
-  console.log(circleSize.value)
-  document.getElementsByClassName("chart-container")[0].style.width = `${circleSize.value}px`
-  document.getElementsByClassName("chart-container")[0].style.height = `${circleSize.value}px`
+    console.log(map, map.value.center[0])
+    zoom.value = newZoom
+
+    redrawCharts()
 }
-      
 
+const initialLat = 48.879411
 
+const getCircleSizeInPixels = (circleSizeInMeters) => {
+    const lat = map.value ? map.value.center[0] : initialLat
+    const metersPerPx = 156543.03392 * Math.cos(lat * Math.PI / 180) / Math.pow(2, zoom.value)
+    const size = circleSizeInMeters / metersPerPx
+    return size + 220;
+}
+
+const circleSizeInPixels = ref(getCircleSizeInPixels())
+
+const redrawCharts = () => {
+    circleSizeInPixels.value = getCircleSizeInPixels();
+    setTimeout(() => {
+        addPie()
+    }, 100);
+}
+
+const circlePositionUpdated = (pie, newPosition) => {
+    console.log(pie, newPosition)
+    pie.latitude = newPosition.lat
+    pie.longitude = newPosition.lng
+}
+
+watch(pies, (newVal) => {
+    console.log("pies update", newVal)
+    redrawCharts()
+})
+
+const selectPie = (index) => {
+    selectedPie.value = index
+    redrawCharts()
+}
+
+const valChange = (selectedPieIndex, dataIndex) => {
+    const pie = pies[selectedPieIndex]
+    const sum = pie.data.reduce((acc, item) => acc + item.value, 0)
+    const nextData = pie.data[(dataIndex + 1) % pie.data.length]
+    nextData.value += 100 - sum
+    redrawCharts()
+}
+const selectedPie = ref(0)
 </script>
 
 <template>
     <!-- <img src="src/assets/paris_bien_etre.jpg" /> -->
     <div id="map-container">
-    <l-map ref="map" :zoom="zoom" :center="[48.913749, 2.225493]" @update:zoom="zoomUpdated">
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></l-tile-layer>
-      <l-marker :lat-lng="[48.879411, 2.290932]">
-        <l-icon
-          :icon-anchor="[200/2, 200/2]"
-          class-name="someExtraClass"
-        >
-          <div class="chart-container">
-            <canvas id="pie" ref="pie"></canvas>
-          </div>
-        </l-icon>
-      </l-marker>
-    </l-map>
-  </div>
+        <l-map ref="map" :zoom="zoom" :center="[initialLat, 2.225493]" @update:zoom="zoomUpdated">
+            <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+                name="OpenStreetMap"></l-tile-layer>
+            <l-image-overlay url="/src/assets/paris_bien_etre_small.jpg" :bounds="[[48.913, 2.205], [48.802, 2.428]]" />
+            <l-marker v-for="(pie, index) in pies" :key="pie.title" :lat-lng="[pie.latitude, pie.longitude]"
+                :draggable="true" @update:lat-lng="position => circlePositionUpdated(pie, position)"
+                @click="() => selectPie(index)">
+                <l-icon :icon-size="[getCircleSizeInPixels(pie.sizeInMeters), getCircleSizeInPixels(pie.sizeInMeters)]"
+                    :icon-anchor="[getCircleSizeInPixels(pie.sizeInMeters) / 2, getCircleSizeInPixels(pie.sizeInMeters) / 2]"
+                    class-name="someExtraClass">
+                    <div class="chart-container">
+                        <canvas :id="pie.title" ref="pie"></canvas>
+                    </div>
+                </l-icon>
+            </l-marker>
+        </l-map>
+        <p>Selected pie : {{ pies[selectedPie].title }}</p>
+
+        <label> Circle size in meters</label>
+        <input id="size_in_meters" type="number" min="300" max="1000" step="3"
+            v-model.number="pies[selectedPie].sizeInMeters" />
+        <output>{{ pies[selectedPie].sizeInMeters }}</output>
+        <div v-for="(item, index) in pies[selectedPie].data" :key="item.label">
+
+            <label>{{ item.label }}, {{ index }}</label>
+            <input type="number" min="0" max="100" step="1" v-model.number="item.value"
+                @change="(e) => valChange(selectedPie, index)" />
+            <output>{{ item.value }}</output>
+        </div>
+        <pre>{{ pies }}</pre>
+    </div>
 </template>
 
 <style>
@@ -118,9 +192,12 @@ div {
     height: 80vh;
     width: 100vw;
 }
-.chart-container{
-  border: 2px solid red;
-  height: 200px;
-  transition: width 0.5s, height 0.5s;
+
+.chart-container {
+    transition: width 0.5s, height 0.5s;
 }
-</style>
+
+canvas {
+    border: 1px solid red;
+    cursor:move;
+}</style>
